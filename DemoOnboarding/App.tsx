56b94ac01456ad56519699085ff82e073d25620e
Cosmin <oroscosmin09@gmail.com>
2024-02-27 11:01:58 +0200

    user store provider

--- DemoOnboarding/App.tsx
+++ DemoOnboarding/App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import OnboardingScreen from './screens/OnboardingScreen';
+import { UserStoreProvider } from './contexts/UserContext';
 
 export default function App() {
-  return <OnboardingScreen />;
+  return (
+    <UserStoreProvider>
+      <OnboardingScreen />
+    </UserStoreProvider>
+  );
 }
 
 
